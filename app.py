b64e6dd7b667a2499c3ea8ac5201a4c6f606daff
Ufuoma <99745782+ucmt10@users.noreply.github.com>
2026-03-10 23:15:59 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #Imports 
-from flask import Flask, render_template 
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)           # creates the Flask application
 
@@ -7,6 +7,28 @@ app = Flask(__name__)           # creates the Flask application
 @app.route("/")                 # creates a route. "/" is the homepage
 def home():                     
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["GET", "POST"])               
+def upload(): 
+
+    if request.method == "POST":
+
+        if "dataset" not in request.files:
+            print("No file part")
+            return render_template("upload.html")
+        
+        file = request.files["dataset"] # when file is submitted Flask stores uploaded files in request.files
+
+        if file.filename == "":
+            print("No file selected")   # testing, when user uploads a file, the terminal will show the file name
+            return render_template("upload.html")
+        
+        print("Uploaded file:", file.filename)
+
+    return render_template("upload.html")
+
+
 
 
 if __name__ == "__main__":
